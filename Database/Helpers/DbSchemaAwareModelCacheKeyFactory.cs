41d3d128faeeb4c549871920dd3ebeb8e90280d6
Mohammad Abu Musa Rabiul <mohammad.rabiul@tokeninc.com>
2021-09-22 12:27:38 +0300

    cleaning

--- Database/Helpers/DbSchemaAwareModelCacheKeyFactory.cs
+++ Database/Helpers/DbSchemaAwareModelCacheKeyFactory.cs
@@ -5,7 +5,6 @@ namespace BlogApi.Database.Helpers
 {
     public class DbSchemaAwareModelCacheKeyFactory : IModelCacheKeyFactory
     {
-        /// <inheritdoc />
         public object Create(DbContext context)
         {
             return new { Type = context.GetType(), Schema = context is IDbContextSchema schema ? schema.Schema : null };
